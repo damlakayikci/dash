4202acc376dbcf437645dfc80a9b07095da84854
damlakayikci <damla.kayikci@boun.edu.tr>
2023-12-06 12:57:49 +0300

    add trim function

---
+++ trim.h
@@ -0,0 +1,10 @@
+#ifndef TRIM_H
+#define TRIM_H
+
+#include <stdio.h> // needed for printf()
+#include <string.h> // needed for strlen()
+#include <ctype.h> // needed for isspace()
+
+char *trim(char *s);
+
+#endif
