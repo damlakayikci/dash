86df9da7274c2078a80ad26676c09bcc7934c1a5
damlakayikci <damla.kayikci@boun.edu.tr>
2023-12-06 08:40:52 +0300

    changed find_command

---
+++ find_command.c
@@ -0,0 +1,58 @@
+#include <dirent.h> // opendir, readdir, closedir. DIR
+#include <sys/stat.h> // stat, struct stat
+#include <string.h> // needed for strlen()
+#include <stdio.h> // needed for printf()
+#include "find_command.h"
+
+extern int *command_found; 
+extern char *found_path;
+
+void trimNewline(char *str) {
+    int len = strlen(str);
+    if (len > 0 && str[len - 1] == '\n') {
+        str[len - 1] = '\0';
+    }
+}
+
+
+void findFilesRecursively(char *baseDirectory, char *file) {
+    char path[1000];
+    struct dirent *dp;
+    DIR *dir = opendir(baseDirectory);
+
+    trimNewline(file);
+
+    printf("Searching for %s in %s\n", file, baseDirectory);
+    // Unable to open directory stream
+    if (!dir) {
+       // printf("Unable to open directory stream\n");
+        return;
+        }
+
+    while ((dp = readdir(dir)) != NULL) {
+        if (strcmp(dp->d_name, ".") != 0 && strcmp(dp->d_name, "..") != 0) {
+            // Construct new path from our base path
+            strcpy(path, baseDirectory);
+            strcat(path, "/");
+            strcat(path, dp->d_name);
+
+            struct stat path_stat;
+            stat(path, &path_stat);
+            if (S_ISREG(path_stat.st_mode)) {      
+          
+                if (strcmp(dp->d_name, file) == 0) {
+                    // printf("File: %s\n", path);
+                    *command_found = 1;
+                    strcpy(found_path, path);
+                    return;
+                }
+              // printf("File: %s\n", path);
+            } else if (S_ISDIR(path_stat.st_mode)) {
+              // printf("Directory: %s\n", path);
+                findFilesRecursively(path, file);
+            }
+        }
+    }
+
+    closedir(dir);
+}
