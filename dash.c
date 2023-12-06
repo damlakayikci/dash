bcb5d8cf417cf503d249bdf18bef5a1b2adc7385
damlakayikci <damla.kayikci@boun.edu.tr>
2023-12-06 21:32:52 +0300

    update dash.c

--- dash.c
+++ dash.c
@@ -2,6 +2,7 @@
 #include <string.h> // needed for strtok()
 #include <stdio.h> // needed for printf()
 #include <unistd.h> // needed for fork(), exec()
+#include <pwd.h>    // needed for getpwuid()
 #include "find_command.h"
 #include "trim.h"
 
@@ -24,12 +25,14 @@ int main(){
     // allocate memory
     command_found = malloc(sizeof(int));
 
+
+
     do{ 
         // initialize variables
         *command_found = 0;
         found_path = (char *) malloc(1024 * sizeof(char));
 
-        // <<<<<<<<<<<<<<<<<< PATH variable ------------------
+        // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< PATH variable --------------------------------------
         char *original_path = strdup(getenv("PATH")); // !!! PATH burada farkli sirada geliyor
 
         // copy path to a new variable to avoid strtok() changing it
@@ -42,64 +45,88 @@ int main(){
             paths[path_count++] = path_token;
             path_token = strtok(NULL, ":");
         }
-        // ------------------ PATH variable >>>>>>>>>>>>>>>>>>
+        // -------------------------------------- PATH variable >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 
-        // Hardcoded for now TODO : change this
-        printf("damlakayikci@Damlas-MacBook-Pro.local /Users/damlakayikci/Desktop/cmpe/okul/cmpe322/shell-project --- ");
+        // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< user info ------------------------------------------
+        // Get current working directory
+        char cwd[1024];
+        getcwd(cwd, sizeof(cwd));
         
+        // Get hostname
+        char hostname[1024];
+        gethostname(hostname, sizeof(hostname));
 
-        // <<<<<<<<<<<<<<<<<<<<<< Input ----------------------
+        // Get username
+        uid_t uid = geteuid(); // 501
+        struct passwd *pw = getpwuid(uid);
+
+        if (pw) {
+        // Access and print the username field
+            printf("%s@%s %s --- ", pw->pw_name, hostname, cwd);
+        } else {
+            // TODO : error handler
+            fprintf(stderr, "Error retrieving password entry.\n");
+            continue;
+        } 
+        // ------------------------------------------ user info >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+        // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< Input ------------------------------------------
         // Get input script
         char input[1024];
         fgets(input, sizeof(input), stdin);
 
-        // parse input
+        // trim input
         char *token = input;
         token = trim(token);
       
-
-        // if token length is 0, there is no input
+        // if token length is 0, there is no input. Ignore the rest
         if (strlen(token) == 0) {
+            // TODO : remove this
             printf("________________ No input\n");
+            // TODO : remove this
             continue;
         }
 
+        // tokenize input
         token = strtok(token, " ");
-        printf("Input after2 is %s\n", token);
         arg_count = 0;
 
         // get the first token as command and check if it is exit
         command = token;
-        printf("Command before is %s\n", command);
         command = trim(command);
-        printf("Command after is %s\n", command);
         comparison = strcmp(command, "exit"); 
-        printf("Comparison is %d\n", comparison);
 
 
         // if command is not exit
         if (comparison) {
             while (token != NULL) {
-                // printf("Token is %s\n", token);
+                // trim token
                 token = trim(token);
+
                 // if length of token is 0, ignore it
                 if (strlen(token) != 0) {
                     args[arg_count++] = token;
                 }
+
+                // get next token
                 token = strtok(NULL, " ");
             }
 
+            // null terminate args
+            args[arg_count] = NULL;
+
             // TODO : remove this
+            printf("------------------------------ HERE ARE THE ARGUMENTS ------------------------------\n");
             printf("Arg count is %d\n", arg_count);
             for (int i = 0; i < arg_count; i++) {
                 printf("Token %d is %s\n", i, args[i]);
-                printf("Length of token %d is %lu\n", i, strlen(args[i]));
+               // printf("Length of token %d is %lu\n", i, strlen(args[i]));
             }
             // TODO : remove this
 
-        
-            // ---------------------- Input >>>>>>>>>>>>>>>>>>>>>>
+
+            // ------------------------------------------ Input >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
                
             // traverse through paths
             for (int i = 0; i < path_count; i++) {
@@ -114,33 +141,56 @@ int main(){
                     break;
                 }
             }
-            // <<<<<<<<<<<<<<<<<<<<<<< Execute -------------------
-            // pid_t pid;
-            // pid = fork();
+            // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< Execute ------------------------------------------
 
-            // if (pid < 0)
-            // {
-            //     fprintf(stderr, "Fork Failed");
-            //     return 1;
-            // }
-            // else if (pid == 0)
-            // {
-            //     execlp(found_path, command, NULL);
-            // }
-            // else
-            // {
-            //     wait(NULL);
-            // }
-            // ----------------------- Execute >>>>>>>>>>>>>>>>>>>>
+            // --------------------- cd ---------------------
+            if (strcmp(command, "cd") == 0) {
+                
+                if (arg_count > 2){
+                    // TODO : error handler
+                    fprintf(stderr, "cd: too many arguments\n");
+                }
+                else if (arg_count == 2){
+                    int result = chdir(args[1]);
+                    if (result == -1){
+                        // TODO : error handler
+                        fprintf(stderr, "cd: no such file or directory: %s\n", args[1]);
+                    }
+                }
+                else{
+                    char *home = getenv("HOME");
+                    int result = chdir(home);
+                    if (result == -1){
+                        // TODO : error handler
+                        fprintf(stderr, "cannot get environment variable HOME\n");
+                    }
+                }
+                continue;
+            }
+            // --------------------- cd ---------------------
 
+            pid_t pid;
+            pid = fork();
+
+            if (pid < 0){
+                // TODO : error handler
+                fprintf(stderr, "Fork Failed");
+                return 1;
+            }
+            else if (pid == 0){
+               execv(found_path, args);
+               //execvp(command, args);
+            }
+            else{
+                wait(NULL);
+            }
+            // ------------------------------------------ Execute >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
         }
         
-
         // free memory
         free(found_path);
         free(original_path);
-
      
     } while(comparison);
 
